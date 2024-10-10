6a242439b050a830a17847f527b5ab80f03f1805
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-10 20:48:55 +0200

    service updated

--- src/app/services/leagues.service.ts
+++ src/app/services/leagues.service.ts
@@ -21,5 +21,19 @@ export class LeaguesService {
 
   }
 
+  getLeagueById(id: string): Observable<League>{
+    return this.httpClient.get<League>(`${API_URL}leagues/${id}`)
+  }
+
+  createLeague(league: League): Observable<League>{
+    return this.httpClient.post<League>(`${API_URL}leagues`, league)
+  }
+
+  updateLeague(league: League): Observable<League>{
+    return this.httpClient.put<League>(`${API_URL}leagues/${league.id_league}`, league)
+  }
+
+
+
 
 }
